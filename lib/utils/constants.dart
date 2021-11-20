52dea9552b0464fad63ca00ba503a7cabeb47d54
Yuwono <labspintar@gmail.com>
2021-11-20 22:30:19 +0700

    make constant

---
+++ lib/utils/constants.dart
@@ -0,0 +1,22 @@
+const List<String> monthlist = [
+  "Januari",
+  "Februari",
+  "Maret",
+  "April",
+  "Mei",
+  "Juni",
+  "Juli",
+  "Agustus",
+  "September",
+  "Oktober",
+  "Nopember",
+  "Desember"
+];
+
+const List<int> yearList = [
+  2021,
+  2022,
+  2023,
+  2024,
+  2025,
+];
